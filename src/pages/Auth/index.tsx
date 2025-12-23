7fcbc9d42b8b000f8faebf75fcf35a6149405dea
RondneyLoiola <rondney.loiola11@gmail.com>
2025-12-23 12:26:37 -0300

    pegando dados do usuário logado

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -1,11 +1,32 @@
-import styles from './styles.module.css'
+/** biome-ignore-all lint/correctness/useExhaustiveDependencies: useEffect */
 
-function Auth(){
-    return (
-        <div className={styles.container}>
-            <h1>Loading...</h1>
-        </div>
-    )
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useSearchParams } from 'react-router';
+import styles from './styles.module.css';
+
+function Auth() {
+	const [searchParams] = useSearchParams(); // pega os parametros da url
+
+	async function getUserInfo() {
+		const { data } = await axios.get('http://localhost:4000/auth/callback', {
+			params: {
+				code: searchParams.get('code'),
+			},
+		});
+
+		console.log(data);
+	}
+
+	useEffect(() => {
+		getUserInfo();
+	}, [getUserInfo]);
+
+	return (
+		<div className={styles.container}>
+			<h1>Loading...</h1>
+		</div>
+	);
 }
 
-export default Auth
+export default Auth;
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,27 +1,30 @@
-import { GithubLogoIcon } from "@phosphor-icons/react";
-import axios from "axios";
-import Button from "../../Components/button";
-import styles from './styles.module.css'
+import { GithubLogoIcon } from '@phosphor-icons/react';
+import axios from 'axios';
+import Button from '../../Components/button';
+import styles from './styles.module.css';
 
 function Login() {
-    async function handleAuth() {
-        const { data } = await axios.get('http://localhost:4000/auth')
+	async function handleAuth() {
+		const { data } = await axios.get('http://localhost:4000/auth');
 
-        window.location.href = data.redirectUrl
-    }
+		window.location.href = data.redirectUrl;
+	}
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.content}>
-                <h1>Entre com</h1>
-                <Button onClick={handleAuth} type="button">
-                    <GithubLogoIcon size={24} />
-                    Github
-                </Button>
-                <p>Ao entrar, eu concordo com o Termo de Serviço e Política de Privacidade</p>
-            </div>
-        </div>
-    )
+	return (
+		<div className={styles.container}>
+			<div className={styles.content}>
+				<h1>Entre com</h1>
+				<Button onClick={handleAuth} type="button">
+					<GithubLogoIcon size={24} />
+					Github
+				</Button>
+				<p>
+					Ao entrar, eu concordo com o Termo de Serviço e Política de
+					Privacidade
+				</p>
+			</div>
+		</div>
+	);
 }
 
-export default Login
+export default Login;
